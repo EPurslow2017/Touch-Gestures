602f065bd388c7994b13f31b041913b584bb0c6c
Edd <pure1_16@uni.worc.ac.uk>
2017-11-13 15:34:55 +0000

    aaa
    aaaa

--- www/js/index.js
+++ www/js/index.js
@@ -1,7 +1,12 @@
 <!-- For more info on jQuery Mobile,  touch gestures and other useful events see : http://api.jquerymobile.com/category/events/ -->
 
 $(document).on("pagecreate","#pageone",function(){
-  
+
+document.addEventListener("deviceready", onDeviceReady, false);
+function onDeviceReady() {
+    console.log(navigator.notification);
+}
+    
 	$('#taptext').on("tap",function(){
        var dunno = true;
        var dice = (Math.round(Math.random()*10));
